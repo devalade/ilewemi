d113ca1ea36b4dfec2d070e71e971819818143e5
devalade <aladecharaf23@gmail.com>
2022-05-29 11:14:01 -0400

    fix(student): add UserEntity and StudentEntity to module import

--- api/src/modules/student/student.module.ts
+++ api/src/modules/student/student.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentEntity } from './entities/student.entity';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StudentEntity, UserEntity])],
   controllers: [StudentController],
-  providers: [StudentService]
+  providers: [StudentService],
 })
 export class StudentModule {}
